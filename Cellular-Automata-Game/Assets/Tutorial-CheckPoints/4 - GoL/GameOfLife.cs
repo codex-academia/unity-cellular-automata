cc325a236ed59a4613eb6ebe0ba994849940e5c8
spsarras <psarras.st@gmail.com>
2025-01-11 16:12:45 +0000

    Checkpoint 4, GOL

---
+++ Cellular-Automata-Game/Assets/Tutorial-CheckPoints/4 - GoL/GameOfLife.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CA
+{
+    public partial class MainGoL
+    {
+        public class GameOfLife
+        {
+            bool[][] currentState;
+            bool[][] nextState;
+            public List<Vector3> Cells;
+            private readonly int gridX;
+            private readonly int gridY;
+
+            public GameOfLife(int gridX, int gridY)
+            {
+                this.gridX = gridX;
+                this.gridY = gridY;
+                currentState = new bool[gridX][];
+                nextState = new bool[gridX][];
+                Cells = new List<Vector3>();
+
+                // initialize the arrays
+                for (int i = 0; i < gridX; i++)
+                {
+                    currentState[i] = new bool[gridY];
+                    nextState[i] = new bool[gridY];
+                }
+
+                DrawGlider(10, 10);
+            }
+
+            public void Update()
+            {
+                for (int i = 0; i < gridX; i++)
+                {
+                    for (int j = 0; j < gridY; j++)
+                    {
+                        // count neighbours
+                        int neighbours = 0;
+                        for (int x = -1; x <= 1; x++)
+                        {
+                            for (int y = -1; y <= 1; y++)
+                            {
+                                if (x == 0 && y == 0)
+                                {
+                                    continue;
+                                }
+
+                                int neighbourX = (i + x + gridX) % gridX;
+                                int neighbourY = (j + y + gridY) % gridY;
+
+                                if (currentState[neighbourX][neighbourY])
+                                {
+                                    neighbours++;
+                                }
+                            }
+                        }
+
+                        // Determine next state
+                        if (currentState[i][j])
+                        {
+                            if (neighbours < 2 || neighbours > 3)
+                            {
+                                nextState[i][j] = false;
+                            }
+                            else
+                            {
+                                nextState[i][j] = true;
+                            }
+                        }
+                        else
+                        {
+                            if (neighbours == 3)
+                            {
+                                nextState[i][j] = true;
+                            }
+                        }
+                    }
+                }
+
+                // Update the current state
+                for (int i = 0; i < gridX; i++)
+                {
+                    for (int j = 0; j < gridY; j++)
+                    {
+                        currentState[i][j] = nextState[i][j];
+                    }
+                }
+            }
+
+            public void Draw()
+            {
+                Cells.Clear();
+                for (int i = 0; i < gridX; i++)
+                {
+                    for (int j = 0; j < gridY; j++)
+                    {
+                        if (currentState[i][j])
+                        {
+                            Cells.Add(new Vector3(i, 0, j));
+                        }
+                    }
+                }
+            }
+
+            public void DrawGlider(int i, int j)
+            {
+                SetAlive(i, j);
+                SetAlive(i + 1, j + 1);
+                SetAlive(i + 2, j - 1);
+                SetAlive(i + 2, j);
+                SetAlive(i + 2, j + 1);
+            }
+
+            public void DrawBlinker(int i, int j)
+            {
+                SetAlive(i, j);
+                SetAlive(i, j + 1);
+                SetAlive(i, j + 2);
+            }
+
+            public void SetAlive(int i, int j)
+            {
+                currentState[i][j] = true;
+            }
+        }
+    }
+}
---
+++ Cellular-Automata-Game/Assets/Tutorial-CheckPoints/4 - GoL/MainGoL.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CA
+{
+    public partial class MainGoL : MonoBehaviour
+    {
+        public GameOfLife gameOfLife;
+        private List<GameObject> gameObjects;
+        public GameObject cellPrefab;
+        private Camera main;
+
+        private void Start()
+        {
+            gameOfLife = new GameOfLife(100, 100);
+            gameObjects = new List<GameObject>();
+            main = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Time.frameCount % 10 == 0)
+            {
+                gameOfLife.Update();
+                gameOfLife.Draw();
+                DrawCells(gameOfLife.Cells);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                var ray = main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    var point = hit.point;
+                    gameOfLife.DrawBlinker((int)point.x, (int)point.z);
+                    // Debug.Log($"Cell at {cell.x}, {cell.z} is alive.");
+                    // Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red, 10);
+                }
+            }
+            else if (Input.GetMouseButtonUp(1))
+            {
+                var ray = main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    var point = hit.point;
+                    gameOfLife.DrawGlider((int)point.x, (int)point.z);
+                }
+            }
+        }
+
+        private void DrawCells(List<Vector3> cells)
+        {
+            foreach (var go in gameObjects)
+            {
+                Destroy(go);
+            }
+
+            gameObjects.Clear();
+
+            foreach (var cell in cells)
+            {
+                var block = Instantiate(cellPrefab);
+                block.transform.position = cell;
+                gameObjects.Add(block);
+            }
+        }
+    }
+}
